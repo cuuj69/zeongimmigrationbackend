5cc604696561275706cebd425684bdcc175e1d78
Sassyprogrammer1 <hamzajamshed.cs@gmail.com>
2022-11-28 19:35:47 +0500

    initiate project structure

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import sls from "serverless-http";
+import router from './routes/router.js';
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.options("*", cors());
+app.use("/api", router);
+
+app.listen(3000, () => {
+  console.log("app is runnnig on port 3000");
+});
+
+export const handler = sls(app);
--- src/routes/router.js
+++ src/routes/router.js
@@ -3,11 +3,11 @@ import * as express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    return res.send('Application is running!');
-})
+  return res.send('Application is running!');
+});
 /**
  * define your routes over here
  * router.get('/universities', universitiesController);
  */
 
-export default router;
+ export default router;
