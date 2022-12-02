ad96b5ff8cb97b7388b8b8ac1ae6eb9fa771579d
Sassyprogrammer1 <hamzajamshed.cs@gmail.com>
2022-12-02 12:37:08 +0500

    typescript configured

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
 import sls from "serverless-http";
-import router from './routes/router.js';
+import router from "./routes/router";
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.options("*", cors());
 app.use("/api", router);
---
+++ src/routes/router.ts
@@ -0,0 +1,13 @@
+import * as express from "express";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  return res.send("Application is running!");
+});
+/**
+ * define your routes over here
+ * router.get('/universities', universitiesController);
+ */
+
+export default router;
